34ed02ffb07caefbf7af8ee7899e3e8ae924712e
Amit Gupta <gupta.amit6700@gmail.com>
2022-08-03 14:59:45 +0530

    Swift Inout

---
+++ Basics/InOut/SwiftInout.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// ========== Without inout ========
+struct User {
+    var userId: String?
+    let name: String
+}
+
+func updateUser(user: User) -> User {
+    var user = user
+    user.userId = "100"
+    return user
+}
+let user = User(userId: "50", name: "Amit")
+
+//let updatedUser = updateUser(user: user)
+
+//print(updatedUser.userId)
+
+// ========== With inout ========
+
+func updateUserIO(_ user:inout User) {
+    user.userId = "200"
+}
+
+updateUser(user: user)
+print(user.userId)
